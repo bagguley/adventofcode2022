84fe8a623d93f9b432369a9a83bb9c511b604228
Wayne Bagguley <wayne.bagguley@gmail.com>
2022-12-04 10:14:25 +0000

    Day 4 part 1 solution

---
+++ src/main/kotlin/day4/Part1.kt
@@ -0,0 +1,11 @@
+package day4
+
+fun main() {
+    println(data.map{
+        val a = it[0][0].toInt()
+        val b = it[0][1].toInt()
+        val c = it[1][0].toInt()
+        val d = it[1][1].toInt()
+        a >= c && b <= d || c >= a && d <= b
+    }.count { it })
+}
