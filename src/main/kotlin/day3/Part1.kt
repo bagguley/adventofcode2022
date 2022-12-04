c82a6095645b9d3bbb06e9d1bb613717c8f840fb
Wayne Bagguley <wayne.bagguley@gmail.com>
2022-12-04 10:38:02 +0000

    Day 3 part 1 refactor

--- src/main/kotlin/day3/Part1.kt
+++ src/main/kotlin/day3/Part1.kt
@@ -1,15 +1,20 @@
 package day3
 
 fun main() {
-    println(data.map{it.intersect()}.sumOf{it.sumOf{Part1.priority(it)}})
+    println(Part1.calc(data))
 }
 
 object Part1 {
-    fun priority(c: Char):Int {
+    fun calc(input: List<String>): Int {
+        return input.map{it.intersect()}.sumOf{it.sumOf{priority(it)}}
+    }
+
+    private fun priority(c: Char):Int {
         return if (c.code > 96) (c.code - 96) else c.code - 64 + 26
+    }
+
+    private fun String.intersect(): Set<Char> {
+        return this.substring(0,length/2).toSet().intersect(this.substring(length/2).toSet())
     }
 }
 
-fun String.intersect(): Set<Char> {
-    return this.substring(0,length/2).toSet().intersect(this.substring(length/2).toSet())
-}
