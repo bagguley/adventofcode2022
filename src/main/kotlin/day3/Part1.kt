26923ef731d03785aa07d18ce5d8d6eed59195cc
Wayne Bagguley <wayne.bagguley@gmail.com>
2022-12-04 10:36:09 +0000

    Day 3 part 1 optimise

--- src/main/kotlin/day3/Part1.kt
+++ src/main/kotlin/day3/Part1.kt
@@ -1,17 +1,15 @@
 package day3
 
 fun main() {
-    println(data.sumOf {
-        val l = it.length
-        val left = it.substring(0, l / 2).toCharArray()
-        val right = it.substring(l / 2).toCharArray()
-        val c = left.intersect(right.toSet())
-        c.sumOf { Part1.priority(it) }
-    })
+    println(data.map{it.intersect()}.sumOf{it.sumOf{Part1.priority(it)}})
 }
 
 object Part1 {
     fun priority(c: Char):Int {
         return if (c.code > 96) (c.code - 96) else c.code - 64 + 26
     }
+}
+
+fun String.intersect(): Set<Char> {
+    return this.substring(0,length/2).toSet().intersect(this.substring(length/2).toSet())
 }
