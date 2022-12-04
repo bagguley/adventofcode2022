3a201d3fcfe86116863cf10d46201c15f5371611
Wayne Bagguley <wayne.bagguley@gmail.com>
2022-12-04 10:45:21 +0000

    Day 4 refactor

--- src/main/kotlin/day4/Part2.kt
+++ src/main/kotlin/day4/Part2.kt
@@ -1,11 +1,11 @@
 package day4
 
 fun main() {
-    println(data.map{
+    println(data.count{
         val a = it[0][0].toInt()
         val b = it[0][1].toInt()
         val c = it[1][0].toInt()
         val d = it[1][1].toInt()
         (a in c..d) || (b in c..d) || (c in a..b) || (d in a..b)
-    }.count { it })
+    })
 }
