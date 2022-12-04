f196247dab281cb391659710bf50bde6745ec667
Wayne Bagguley <wayne.bagguley@gmail.com>
2022-12-04 21:58:33 +0000

    Day 3 part 2 in one line

--- src/main/kotlin/day3/Part2.kt
+++ src/main/kotlin/day3/Part2.kt
@@ -2,6 +2,7 @@ package day3
 
 fun main() {
     println(Part2.calc(data))
+    println(Part2.calcOneLine(data))
 }
 
 object Part2 {
@@ -11,5 +12,9 @@ object Part2 {
 
     private fun priority(c: Char):Int {
         return if (c.code > 96) (c.code - 96) else c.code - 64 + 26
+    }
+
+    fun calcOneLine(input: List<String>): Int {
+        return input.map{it.toSet()}.chunked(3).sumOf{it[0].intersect(it[1]).intersect(it[2]).first().code.let{it.and(31)+it.shr(5).and(1).xor(1)*26}}
     }
 }
