d8b3a3af21b0fe122665d4e29e9c23c28ecf09dd
Wayne Bagguley <wayne.bagguley@gmail.com>
2022-12-04 15:46:25 +0000

    Day 3 part 2 refactor

--- src/main/kotlin/day3/Part2.kt
+++ src/main/kotlin/day3/Part2.kt
@@ -1,20 +1,15 @@
 package day3
 
 fun main() {
-    val chunked = data.chunked(3)
-
-    println(chunked.sumOf {
-        val a = it[0].toCharArray().toSet()
-        val b = it[1].toCharArray().toSet()
-        val c = it[2].toCharArray().toSet()
-
-        val z = a.intersect(b).intersect(c)
-        z.sumOf { Part2.priority(it) }
-    })
+    println(Part2.calc(data))
 }
 
 object Part2 {
-    fun priority(c: Char):Int {
+    fun calc(input: List<String>): Int {
+        return input.map{it.toSet()}.chunked(3).sumOf{it[0].intersect(it[1]).intersect(it[2]).first().let{priority(it)}}
+    }
+
+    private fun priority(c: Char):Int {
         return if (c.code > 96) (c.code - 96) else c.code - 64 + 26
     }
 }
